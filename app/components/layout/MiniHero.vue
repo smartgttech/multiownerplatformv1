0e4ce4ec42cbca6ebda351082820e6cd33f2fab3
smartgttech <smart.gttech@gmail.com>
2026-01-30 11:10:08 +0100

    Création du template par defaut. Création des composants Hero, Header, Footer pour toutes les pages publiques. Utilisation du template par defaut sur une page de landing d'exemple. Mise à jour de l'architecture frontend pour plus de scalabilité

---
+++ app/components/layout/MiniHero.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+    // Importations
+    import { ref } from "vue"
+
+    /** Declaration des variables */
+    const actualPageTitle = (setTitle: string) => {
+        
+    }
+
+</script>
+
+<template>
+
+
+</template>
